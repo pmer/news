1cb6af9c95cf60d7f7f55bfa300be5c181e930ce
Paul Merrill <pdm@pdm.me>
2023-01-11 17:24:35 -0800

    Add GitHub Actions to run tests

--- test/hn.cpp
+++ test/hn.cpp
@@ -14,13 +14,18 @@ hnParse(String& response) noexcept;
 void
 testHn() noexcept {
     String out;
-
     expand(out, "a");
     assert_(out == "a");
-
     expand(out, "&#x2F;");
     assert_(out == "/");
-
     expand(out, "a&#x2F;b&#x27;c");
     assert_(out == "a/b'c");
+
+    String response = "<rss version=\"2.0\"><channel><title>Hacker News</title><link>https://news.ycombinator.com/</link><description>Links for the intellectually curious, ranked by readers.</description><item><title>NYC Slice</title><link>https://elkue.com/nyc-slice/</link><pubDate>Wed, 11 Jan 2023 19:00:54 +0000</pubDate><comments>https://news.ycombinator.com/item?id=34342871</comments><description><![CDATA[<a href=\"https://news.ycombinator.com/item?id=34342871\">Comments</a>]]></description></item></channel></rss>";
+    HackerNews* hn = hnParse(response);
+    assert_(hn->errmsg.data == 0);
+    assert_(hn->items.size == 1);
+    assert_(hn->items[0].title == "NYC Slice");
+    assert_(hn->items[0].link == "https://elkue.com/nyc-slice/");
+    assert_(hn->items[0].commentsHref == "https://news.ycombinator.com/item?id=34342871");
 }
